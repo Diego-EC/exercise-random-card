b65ce78643a578b5a7986b81ad7bb680ec81e582
Diego-EC <diegoezquerro@gmail.com>
2020-08-28 18:49:26 +0000

    Finished; but I want to see if I can improve it with flexbox.
    Signed-off-by: Diego-EC <diegoezquerro@gmail.com>

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,34 @@
-/* eslint-disable */
-
-//import "../assets/img/rigo-baby.jpg";
-//import "../assets/img/4geeks.ico";
-//import 'breathecode-dom'; //DOM override to make JS easier to use
-//import "../style/index.scss";
 import "../style/styles.css";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add(generateRandomCardSuit());
+  document.querySelector(".card").innerHTML = generateRandomCardNumber();
 };
+
+function generateRandomCardSuit() {
+  const arrSuits = ["hearts", "spades", "clubs", "diamonds"];
+
+  let indexSuit = Math.floor(Math.random() * arrSuits.length);
+  return arrSuits[indexSuit];
+}
+
+function generateRandomCardNumber() {
+  const arrNumbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let indexNumber = Math.floor(Math.random() * arrNumbers.length);
+  return arrNumbers[indexNumber];
+}
